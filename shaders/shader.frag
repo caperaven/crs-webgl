9188f4b951b4acac52eea2c8d30ba4eb6e40657b
caperaven <caperaven@gmail.com>
2020-06-01 23:14:35 +0200

    get shaders working

--- shaders/shader.frag
+++ shaders/shader.frag
@@ -6,20 +6,23 @@ uniform vec4 u_color;
 uniform float u_time;
 uniform float u_delta;
 uniform vec2 u_resolution;
+uniform vec2 u_mouse;
 
-vec3 colorA = vec3(1.0, 0.0, 0.0);
-vec3 colorB = vec3(0.0, 0.0, 1.0);
-
-vec3 mixOverTime(vec3 cl1, vec3 cl2) {
+vec3 mixOverTime(vec3 cl1, vec3 cl2, float delta) {
     vec3 color = vec3(0.0);
-    float pct = abs(sin(u_time));
+    float pct = abs(sin(delta));
     color = mix(cl1, cl2, pct);
     return color;
 }
 
+vec3 colorA = vec3(1.0, 0.5, 0.0);
+vec3 colorB = vec3(0.0, 0.5, 1.0);
+
 void main() {
     vec2 st = gl_FragCoord.xy/u_resolution;
+    vec3 color = mixOverTime(colorA, colorB, u_time);
+    color.x = cos(st.x);
+    color.y = sin(st.y);
 
-    //gl_FragColor = vec4(mixOverTime(colorA, colorB), 1.0);
-    gl_FragColor = vec4(st.x,st.y,0.0,1.0);
+    gl_FragColor = vec4(color, 1.0);
 }
--- src/crs-webgl.js
+++ src/crs-webgl.js
@@ -3,8 +3,15 @@ import "./crs-webgl-program.js";
 class WebGL extends HTMLElement {
     connectedCallback() {
         this.programs = [];
+        this.pixelRatio = window.devicePixelRatio || 1;
+        this.mouse = {x: 0, y: 0};
+
         this._drawHandler = this._draw.bind(this);
         this._addProgramHandler = this._addProgram.bind(this);
+        this._mouseMoveHander = this._mouseMove.bind(this);
+        this._resizeHandler = this._resize.bind(this);
+
+        window.addEventListener('resize', this._resizeHandler);
 
         this.programElements = this.querySelectorAll("crs-webgl-program");
         this.programElements.forEach(element => element.addEventListener("program-ready", this._addProgramHandler));
@@ -13,10 +20,24 @@ class WebGL extends HTMLElement {
     }
 
     disconnectedCallback() {
+        window.removeEventListener('resize', this._resizeHandler);
+        this.canvas.removeEventListener("mousemove", this._mouseMoveHander);
+
         this._drawHandler = null;
         this._addProgramHandler = null;
+        this._mouseMoveHander = null;
+        this._resizeHandler = null;
         this.canvas = null;
         this.gl = null;
+    }
+
+    _resize() {
+        this.bounds = this.canvas.getBoundingClientRect();
+    }
+
+    _mouseMove(event) {
+        this.mouse.x = (event.clientX - this.bounds.left) * this.pixelRatio;
+        this.mouse.y = this.canvas.height - (event.clientY - this.bounds.top) * this.pixelRatio;
     }
 
     _addProgram(event) {
@@ -39,8 +60,13 @@ class WebGL extends HTMLElement {
         this.canvas.style.height = "100%";
         this.appendChild(this.canvas);
 
-        this.gl = this.canvas.getContext("webgl");
-        this._loadVBO();
+        requestAnimationFrame(() => {
+            this._resize();
+            this.canvas.addEventListener("mousemove", this._mouseMoveHander);
+            this.gl = this.canvas.getContext("webgl");
+            this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+            this._loadVBO();
+        });
     }
 
     _loadVBO() {
@@ -66,6 +92,9 @@ class WebGL extends HTMLElement {
 
             program.u_color = this.gl.getUniformLocation(program, "u_color");
             this.gl.uniform4fv(program.u_color, [0, 1, 0, 1.0]);
+
+            program.u_mouse = this.gl.getUniformLocation(program, "u_mouse");
+            this.gl.uniform2fv(program.u_mouse, [this.mouse.x, this.mouse.y]);
 
             program.u_time = this.gl.getUniformLocation(program, "u_time");
             this.gl.uniform1f(program.u_time, now);
